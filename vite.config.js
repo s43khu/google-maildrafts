c4522fcc8ea1712315aa3112613c2252c5db67c6
shekhu <s43khu@gmail.com>
2024-08-15 13:27:23 +0530

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/anon-chat",
+  base: "/google-maildrafts",
   plugins: [react()],
   //     // http: require.resolve('rollup-plugin-node-builtins'),
   //     // util: require.resolve('rollup-plugin-node-builtins'),
